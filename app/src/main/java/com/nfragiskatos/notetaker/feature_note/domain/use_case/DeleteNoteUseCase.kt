1554e6ef300a23f12e0edf043d4269d2b6eb8867
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-01 18:14:16 -0700

    - created use case for deleting a note

---
+++ app/src/main/java/com/nfragiskatos/notetaker/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.notetaker.feature_note.domain.use_case
+
+import com.nfragiskatos.notetaker.feature_note.domain.model.Note
+import com.nfragiskatos.notetaker.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
