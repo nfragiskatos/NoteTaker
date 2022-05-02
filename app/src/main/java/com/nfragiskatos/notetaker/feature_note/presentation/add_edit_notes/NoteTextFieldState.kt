2517424732b0076c7e08bce4f1eec4a86af2b165
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-02 16:35:07 -0700

    - created event classes for different user actions on the note detail screen - created NoteTextFieldState.kt to handle the more complex state of the title and content text field

---
+++ app/src/main/java/com/nfragiskatos/notetaker/feature_note/presentation/add_edit_notes/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.notetaker.feature_note.presentation.add_edit_notes
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
