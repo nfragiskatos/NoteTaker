8f3edd16aebd6398ffd20011fa3b9ca4da3962c9
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-01 18:15:47 -0700

    - created wrapper class to contain all the different use case for working with a Note. This will be what is injected into the viewmodels

---
+++ app/src/main/java/com/nfragiskatos/notetaker/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,7 @@
+package com.nfragiskatos.notetaker.feature_note.domain.use_case
+
+data class NoteUseCases(
+    val getNotesUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase
+) {
+}
