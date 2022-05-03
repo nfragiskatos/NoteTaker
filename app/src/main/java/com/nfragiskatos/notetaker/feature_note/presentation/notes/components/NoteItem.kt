5c59fe1fa68191e45524f5b0549167526b9783e6
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-02 21:03:28 -0700

    - changed delete icon color to onSurface for better contrast

--- app/src/main/java/com/nfragiskatos/notetaker/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/nfragiskatos/notetaker/feature_note/presentation/notes/components/NoteItem.kt
@@ -108,7 +108,8 @@ fun NoteItem(
         ) {
             Icon(
                 imageVector = Icons.Default.Delete,
-                contentDescription = "Delete Note"
+                contentDescription = "Delete Note",
+                tint = MaterialTheme.colors.onSurface
             )
         }
     }
