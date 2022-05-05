a5a131d2c5c14e125a45d85cf5443df69345ff61
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2022-05-04 17:32:36 -0700

    - created TestAppModule.kt to use in tests. Contains an injectable mock version for the database - created subclass of AndroidJUnitRunner to override newApplication function so we can specify the use of HiltTestApplication

---
+++ app/src/androidTest/java/com/nfragiskatos/notetaker/di/TestAppModule.kt
@@ -0,0 +1,45 @@
+package com.nfragiskatos.notetaker.di
+
+import android.app.Application
+import androidx.room.Room
+import com.nfragiskatos.notetaker.feature_note.data.data_source.NoteDatabase
+import com.nfragiskatos.notetaker.feature_note.data.repository.NoteRepositoryImpl
+import com.nfragiskatos.notetaker.feature_note.domain.repository.NoteRepository
+import com.nfragiskatos.notetaker.feature_note.domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            NoteDatabase::class.java,
+        )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotesUseCase = GetNotesUseCase(repository),
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNoteUseCase = AddNoteUseCase(repository),
+            getNoteUseCase = GetNoteUseCase(repository)
+        )
+    }
+}
